13e4b88ec51704d91555dea2d2bebf001457a2f1
Jason Baker <amnorvend@gmail.com>
2014-01-12 17:18:12 -0800

    Add posts and other fixes.

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ class MainPage(webapp2.RequestHandler):
 
   def get(self):
     self.response.write(index_text)
+    self.response.headers['Cache-Control'] = 'max-age: 86400'
 
 app = webapp2.WSGIApplication([('/.*', MainPage)],
                               debug=True)
--- src/static/maincontroller.js
+++ src/static/maincontroller.js
@@ -9,6 +9,10 @@ var ngblog = {};
     $routeProvider.when('/posts/:postId', {
       controller: PostCtrl,
       template: '<div ng-include="currentPage"></div>'
+    });
+
+    $routeProvider.when('/', {
+      templateUrl: '/post-templates/home.ng'
     })
   })
 
@@ -17,25 +21,45 @@ var ngblog = {};
     $scope.currentPage = template;
   }
 
-  mod.directive('ngblogLink', ['$rootScope', '$location', function($rootScope, $location) {
+  mod.directive('ngblogLink', function($location) {
     return {
       replace: true,
       restrict: 'E',
+      link: function(scope, element, attrs) {
+        scope.postUrl = "/posts/" + scope.post;
+        function changeClass() {
+          if ($location.path() === scope.postUrl) {
+            element.addClass('active-item');
+          } else {
+            element.removeClass('active-item');
+          }
+        } 
+        changeClass($location.path());
+        scope.$on('$locationChangeSuccess', function(e, newUrl) {
+          changeClass(newUrl);
+        });
+      },
       scope: {
         post: '@postId',
         activePost: '='
       },
-      template: '<li class="menu-li"><a ng-href="/posts/{{post}}" ng-transclude></a></li>',
+      template: '<li class="menu-li"><a ng-href="{{postUrl}}" ng-transclude></a></li>',
       transclude: true
     };
-  }]);
+  });
 
   mod.directive('ngblogList', function() {
     return {
       replace: true,
       restrict: 'E',
-      template: '<ul ng-transclude></ul>',
+      template: '<ul class="postlist" ng-transclude></ul>',
       transclude: true
     }
-  })
+  });
+
+  mod.directive('ngblogSideNav', function() {
+    return {
+      templateUrl: '/static/'
+    }
+  });
 })();
